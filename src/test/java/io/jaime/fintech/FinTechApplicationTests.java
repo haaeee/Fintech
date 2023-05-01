1c5328c6e33639785e0f359ce9000462cebc67f2
haaeee <chungjm0711@gmail.com>
2023-05-01 18:38:10 +0900

    chore: 프로젝트 setUp

---
+++ src/test/java/io/jaime/fintech/FinTechApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jaime.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
